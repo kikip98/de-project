85d93416da6536ac676364c8aefc74a1125c1713
kikip98 <uceikp0@ucl.ac.uk>
2022-04-20 20:26:43 +0300

    Add commentary

--- spark/scripts/mllib_spark.py
+++ spark/scripts/mllib_spark.py
@@ -2,25 +2,22 @@
 import pyspark
 import pandas as pd
 import numpy as np
+import psycopg2
 from pyspark.sql import SparkSession
-from pyspark.sql import SQLContext
-from pyspark import SparkContext, SparkConf
-from pyspark.sql.functions import when,split,col,udf,from_unixtime, unix_timestamp, regexp_replace
-from pyspark.ml.feature import OneHotEncoder, StringIndexer, VectorAssembler
-from pyspark.sql.types import IntegerType
-
+from pyspark.sql.functions import when,col,udf, regexp_replace
+from pyspark.ml.feature import StringIndexer, VectorAssembler
+from pyspark.sql.types import IntegerType, FloatType
+from pyspark.ml.classification import RandomForestClassifier
 from pyspark.sql.functions import *
 from pyspark.ml import Pipeline, PipelineModel
 import os
 import boto3
+from pyspark.ml.evaluation import MulticlassMetrics, MulticlassClassificationEvaluator
+import pyspark.sql.functions as F
 
 #Building Spark ssession
 spark = SparkSession   .builder     .appName("PySpark App")     .config("spark.jars", "/home/master/jar/postgresql-42.3.2.jar")     .getOrCreate()
 
-
-
-import psycopg2
-import pandas as pd
 
 # Connect to DB
 engine = psycopg2.connect(
@@ -30,17 +27,18 @@ engine = psycopg2.connect(
     host="kikipng.ckoss6hrgu4d.eu-west-2.rds.amazonaws.com",
     port=5432)
 
+# Query data
 query = """select * from airline_postgres_schema.skytrax_reviews"""
 df_skytrax  = pd.read_sql(query, engine)
 dfs_skytrax = spark.createDataFrame(df_skytrax)
 
 
+# Print Schema
 dfs_skytrax.printSchema()
 
 
 
 # Feature Engineering
-
 def get_one_hot_encodings(dfs, column_name):   
     '''
     Input: spark dataframe and name of column we want to one-hot-encode
@@ -75,74 +73,57 @@ dfs_skytrax = dfs_skytrax.withColumn('recommendation', regexp_replace('recommend
 
 
 
-# Model
-
+# Prepare the data for the model
 numericCols = [item[0] for item in dfs_skytrax.dtypes if item[1]== 'bigint' or item[1]== 'int' or item[1]== 'double' ]#[1:]
-
-
-
 assembler = VectorAssembler(inputCols=numericCols, outputCol="features")
-
-
 dfs_new = assembler.transform(dfs_skytrax)
-
-
-
-
-
 label_stringIdx = StringIndexer(inputCol = 'recommendation', outputCol = 'labelIndex')
 dfs_new = label_stringIdx.fit(dfs_new).transform(dfs_new)
 
 
-
+# Split the data to train and test
 train, test = dfs_new.randomSplit([0.7, 0.3], seed = 2018)
 print("Records for training dataset: " + str(train.count()))
 print("Records for test dataset: " + str(test.count()))
 
 
-
-
-from pyspark.ml.classification import RandomForestClassifier
-
+# Fit the train data to the model
 rf = RandomForestClassifier(featuresCol = 'features', labelCol = 'labelIndex')
 rfModel = rf.fit(train)
 
 
+# Create a function that saves the data to s3 bukcet
 def uploadDirectory(path):
     session = boto3.Session(
         aws_access_key_id='AKIA2X2ER6BK2I4N3NO4',
         aws_secret_access_key='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'
     )
-    s3C = session.resource('s3').Bucket('airline-project-kikipng')
+    s3 = session.resource('s3').Bucket('airline-project-kikipng')
     bucketname = 'airline-project-kikipng'
     for root,dirs,files in os.walk(path):
         for file in files:
             print(file)
-            s3C.upload_file(os.path.join(root,file),os.path.join(root,file))
+            s3.upload_file(os.path.join(root,file),os.path.join(root,file))
 
 
+# Save the model locally
 rfModel.write().overwrite().save('model')
+
+# Transfer the model to s3 busket
 uploadDirectory('model')
 
 
+# Transform the test data 
 predictions = rfModel.transform(test)
 predictions.select('labelIndex', 'rawPrediction', 'prediction', 'probability').show(10)
 
-
-
-from pyspark.ml.evaluation import MulticlassClassificationEvaluator
-
+# Evaluate the model
 evaluator = MulticlassClassificationEvaluator(labelCol="labelIndex", predictionCol="prediction")
 accuracy = evaluator.evaluate(predictions)
 print("Accuracy = %s" % (accuracy))
 print("Test Error = %s" % (1.0 - accuracy))
 
-
-
-from pyspark.mllib.evaluation import MulticlassMetrics
-from pyspark.sql.types import FloatType
-import pyspark.sql.functions as F
-
+# Create a confusion matrix
 preds_and_labels = predictions.select(['prediction','labelIndex']).withColumn('labelIndex', F.col('labelIndex').cast(FloatType())).orderBy('prediction')
 preds_and_labels = preds_and_labels.select(['prediction','labelIndex'])
 metrics = MulticlassMetrics(preds_and_labels.rdd.map(tuple))
